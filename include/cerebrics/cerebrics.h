8c06153d44cc2e02cd863a81c52b674b4fb31b78
William Timimi <biltimi@gmail.com>
2026-06-17 16:09:51 +0100

    style add section comments to cerebrics.h and cerebrics.c

--- include/cerebrics/cerebrics.h
+++ include/cerebrics/cerebrics.h
@@ -20,18 +20,22 @@ typedef struct CBR_Tensor {
     int requires_grad;
 } CBR_Tensor;
 
+// ways to create and destroy a tensor
 CBR_Tensor *cbr_tensor_create(int ndim, const int *shape, int requires_grad);
 CBR_Tensor *cbr_tensor_from_data(const float *data, int ndim, const int *shape, int requires_grad);
 void cbr_tensor_free(CBR_Tensor *t);
 
-CBR_Status cbr_tensor_fill(CBR_Tensor *t, float value);
-CBR_Status cbr_tensor_get(const CBR_Tensor *t, const int *indices, float *out_value);
-CBR_Status cbr_tensor_set(CBR_Tensor *t, const int *indices, float value);
-
+// Get the different properties of a tensor
 int cbr_num_el(CBR_Tensor *t);
 float *cbr_data(CBR_Tensor *t);
 float *cbr_grad(CBR_Tensor *t);
 int cbr_shape(CBR_Tensor *t, int axis);
+
+// functions to get and set data in a tensor
+CBR_Status cbr_tensor_fill(CBR_Tensor *t, float value);
+CBR_Status cbr_tensor_get(const CBR_Tensor *t, const int *indices, float *out_value);
+CBR_Status cbr_tensor_set(CBR_Tensor *t, const int *indices, float value);
+void cbr_tensor_zero_grad(CBR_Tensor *t);
 
 #ifdef __cplusplus
 }
